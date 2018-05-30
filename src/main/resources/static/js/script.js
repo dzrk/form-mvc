20a24fbe5ebca4cf15303e2f0ce1932655083ab8
dzrk <derrick.phung@hotmail.com>
2018-05-30 15:17:52 +1000

    layout

---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,13 @@
+
+$('.navbar-toogle bar').click(function() {
+   $(this).toggleClass('active');
+   $('#myNav').toggleClass('open');
+  });
+
+
+// Close after clicking on a li
+
+//$('#nav-overlay li').on('click', function(){
+//    $("#nav-overlay").hide();
+//    $("#button-toggle").removeClass("active");
+//});
